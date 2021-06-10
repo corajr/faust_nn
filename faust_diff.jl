e45d7301bb2c681b93958596710b89ad05767462
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2021-06-10 13:08:44 -0400

    learnable faust layer

--- faust_diff.jl
+++ faust_diff.jl
@@ -8,58 +8,58 @@ import Zygote
 block_size = 2048
 samplerate = 16000
 
-osc = Faust.compile("""
-import("stdfaust.lib");
-freq = 10 ^ hslider("log10_freq", 2.0, 2.0, 3.0, 0.001);
-gain = hslider("gain", 0.0, 0.0, 1.0, 0.001);
-process = os.oscs(freq) * gain;
-""")
-
-function oracle(x)
-    Faust.init(osc, block_size=block_size, samplerate=samplerate)
-    unsafe_store!(osc.ui.paths["/score/log10_freq"], x[1])
-    unsafe_store!(osc.ui.paths["/score/gain"], x[2])
-    vec(Faust.compute(osc))
-end
-
 process = Faust.compile("""
 import("stdfaust.lib");
 
 freq = 10 ^ hslider("log10_freq", 2.0, 2.0, 3.0, 0.001);
 Q = 200.0;
 gain = hslider("gain", 0.0, 0.0, 1.0, 0.001);
-process = no.noise : fi.resonbp(freq, Q, gain);
+process = fi.resonbp(freq, Q, gain);
 """)
 
-function f(x::Vector{Float32})
+function f(x::Vector{Float32}, params::Vector{Float32})
     Zygote.ignore() do
         Faust.init(process, block_size=block_size, samplerate=samplerate)
-        unsafe_store!(process.ui.paths["/score/log10_freq"], x[1])
-        unsafe_store!(process.ui.paths["/score/gain"], x[2])
+        process.inputs = reshape(x, block_size, 1)
+        unsafe_store!(process.ui.paths["/score/log10_freq"], params[1])
+        unsafe_store!(process.ui.paths["/score/gain"], params[2])
         vec(Faust.compute(process))
     end
 end
 
-function f(x::Matrix{Float32})
-    mapslices(f, x, dims = [1])
+function f(x::Matrix{Float32}, params::Vector{Float32})
+    mapslices(v -> f(v, params), x, dims=[1])
 end
 
-Zygote.@adjoint f(x) = f(x), ȳ -> (FiniteDifferences.j'vp(FiniteDifferences.central_fdm(5, 1), f, ȳ, x),)
+struct FaustLayer
+    params::Vector{Float32}
+end
+
+function FaustLayer(in::Integer; init = in -> ones(Float32, in))
+    return FaustLayer(init(in))
+end
+
+function (m::FaustLayer)(x)
+    f(x, m.params)
+end
+
+Flux.@functor FaustLayer
+
+Zygote.@adjoint f(x, p) = f(x, p), ȳ -> (
+    nothing,
+    FiniteDifferences.j′vp(FiniteDifferences.central_fdm(5, 1), p -> f(x, p), ȳ, p)[1],
+)
 Zygote.refresh()
 
-model = Flux.Chain(
-    Flux.Dense(2, 2),
-    f,
-)
+model = FaustLayer(2)
 ps = Flux.params(model)
 loss(x, y) = FaustNN.harmonic_loss(model(x), y)
 
-xs = vec([collect(p) for p in Iterators.product(0.0f0:0.1f0:1f0, 0.5f0:0.1f0:1f0)])
-ys = oracle.(xs)
-xs = reduce(hcat, xs)
-ys = reduce(hcat, ys)
+original_param = [0.5f0, 0.5f0]
+xs = 2 * rand(Float32, block_size, 50) .- 1
+ys = mapslices(x -> f(x, original_param), xs, dims=[1])
 
-opt = Flux.ADAM()
+opt = Flux.Descent()
 i = 0
 
 function display_loss(loss_total)
