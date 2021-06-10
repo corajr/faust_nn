619f0a913e7e469bdde29a5594be1fffce98e7e4
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2021-06-10 10:08:57 -0400

    end-to-end diff

--- faust_diff.jl
+++ faust_diff.jl
@@ -5,7 +5,7 @@ import Flux
 import FFTW
 import Zygote
 
-block_size = 1024
+block_size = 2048
 samplerate = 16000
 
 osc = Faust.compile("""
@@ -15,10 +15,10 @@ gain = hslider("gain", 0.0, 0.0, 1.0, 0.001);
 process = os.oscs(freq) * gain;
 """)
 
-function oracle(ps)
+function oracle(x)
     Faust.init(osc, block_size=block_size, samplerate=samplerate)
-    unsafe_store!(osc.ui.paths["/score/log10_freq"], ps[1])
-    unsafe_store!(osc.ui.paths["/score/gain"], ps[2])
+    unsafe_store!(osc.ui.paths["/score/log10_freq"], x[1])
+    unsafe_store!(osc.ui.paths["/score/gain"], x[2])
     vec(Faust.compute(osc))
 end
 
@@ -31,33 +31,33 @@ gain = hslider("gain", 0.0, 0.0, 1.0, 0.001);
 process = no.noise : fi.resonbp(freq, Q, gain);
 """)
 
-function f(ps::Vector{Float32})
+function f(x::Vector{Float32})
     Zygote.ignore() do
         Faust.init(process, block_size=block_size, samplerate=samplerate)
-        unsafe_store!(process.ui.paths["/score/log10_freq"], ps[1])
-        unsafe_store!(process.ui.paths["/score/gain"], ps[2])
+        unsafe_store!(process.ui.paths["/score/log10_freq"], x[1])
+        unsafe_store!(process.ui.paths["/score/gain"], x[2])
         vec(Faust.compute(process))
     end
 end
 
-function f(ps::Matrix{Float32})
-    mapslices(f, ps, dims = [1])
+function f(x::Matrix{Float32})
+    mapslices(f, x, dims = [1])
 end
 
-∇f(p) = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(5, 1), f, p)
-
-Zygote.@adjoint f(p) = f(p), ȳ -> (ȳ' * ∇f(p)[1],)
+Zygote.@adjoint f(x) = f(x), ȳ -> (FiniteDifferences.j'vp(FiniteDifferences.central_fdm(5, 1), f, ȳ, x),)
 Zygote.refresh()
 
-model = f
+model = Flux.Chain(
+    Flux.Dense(2, 2),
+    f,
+)
 ps = Flux.params(model)
+loss(x, y) = FaustNN.harmonic_loss(model(x), y)
 
-xs = vec([collect(p) for p in Iterators.product(0:0.1f0:1, 0:0.1f0:1)])
+xs = vec([collect(p) for p in Iterators.product(0.0f0:0.1f0:1f0, 0.5f0:0.1f0:1f0)])
 ys = oracle.(xs)
 xs = reduce(hcat, xs)
 ys = reduce(hcat, ys)
-
-loss(x, y) = harmonic_loss(f(x), y)
 
 opt = Flux.ADAM()
 i = 0
@@ -76,4 +76,4 @@ function evalcb()
 end
 
 d_batch = Flux.Data.DataLoader((xs, ys))
-Flux.@epochs 10 Flux.train!(loss, ps, d_batch, opt, cb = evalcb)
+Flux.@epochs 20 Flux.train!(loss, ps, d_batch, opt, cb = evalcb)
--- src/harmonic_loss_fns.jl
+++ src/harmonic_loss_fns.jl
@@ -12,7 +12,7 @@ Signal is considered 0 if we sample before it starts.
 """
 function lags(x, n, t)
     x_sampled = zeros(n)
-    for i = 0:n-1
+    for i = 0:n - 1
         t_lag = t - 2^i
         if t_lag > 0
             x_sampled[i + 1] = x[t_lag]
@@ -21,25 +21,35 @@ function lags(x, n, t)
     return x_sampled
 end
 
+hann = DSP.Windows.hanning(256, zerophase=true)
+
 """
-    mag_spec(x[, n])
+    mag_spec(x[, n][, hop_size][, window])
 
-Computes the magnitude spectrum of audio signal `x` using FFT size `n`.
+Computes the |STFT| of audio signal `x` using FFT size `n` with `window`.
 
-If `n` is not given, it defaults to 256.
+If `n` is not given, it defaults to 256. `hop_size` is div(n, 2). 
+`window` defaults to the Hann window.
+
+Note that to let Zygote work we must avoid mutating arrays in this
+function, so a window must always be supplied. The length of the
+window overrides the `n` argument.
 
 # Examples
 ```julia-repl
 julia> x = rand(256, 1); Plots.heatmap(log1p.(mag_spec(x)'))
+julia> mag_spec(x, DSP.Windows.hanning(512, zerophase=true))
 ```
 """
-function mag_spec(x, n = 256)
-    hann = DSP.Windows.hanning(n, zerophase=true)
+function mag_spec(x; n=256, hop_size=128, window=hann)
+    if size(window, 1) != n
+        n = size(window, 1)
+    end
     hop_size = div(n, 2)
-    n_windows = div(size(x, 1), n)
+    n_complete_windows = div(size(x, 1), n)
     reduce(hcat, [
-        abs.(FFTW.rfft(x[offset:offset+n-1] .* hann))
-        for offset in 1:hop_size:(n_windows*n)
+        abs.(FFTW.rfft(x[offset:offset + n - 1] .* window))
+        for offset in 1:hop_size:((n_complete_windows * n) - hop_size) 
     ])
 end
 
